37f154e1474e438cf9879454fac15be19b52d948
ldasilva <ldasilva.uoc.edu>
2021-11-14 21:18:46 +0100

    Intalación y subida de imagenes

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,12 +3,21 @@
  * see commented examples below
  */
 
+ import * as bootstrap from 'bootstrap';
+
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 
 /**
  * Write any other JavaScript below
  */
+
+ AOS.init({
+  offset: 20,
+  delay: 100,
+  duration: 1000,
+  easing: 'ease'});
+
 
 +( function() {
   const university = "UOC";
