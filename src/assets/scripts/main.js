6adfeeae1460861feefeec23e9f2546d708986e9
Luiza Clara Da Silva <ldasilva@uoc.edu>
2021-11-21 00:28:59 +0100

    contruccion web

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -11,14 +11,5 @@ import "/node_modules/aos/dist/aos.js";
  * Write any other JavaScript below
  */
 
- AOS.init({
-  offset: 20,
-  delay: 100,
-  duration: 1000,
-  easing: 'ease'});
 
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
